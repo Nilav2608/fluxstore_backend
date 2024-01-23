d3441bde0fe9f665c7a7b06f2a64ae39b0a647ce
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-23 23:34:36 +0530

    making connection with mongoDB Atlas

---
+++ configs/mongoDBconfigs.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+
+const connectionString = "mongodb+srv://fluxstoreDB:zADkiIxwLZpWo1cd@cluster0.dxzadmr.mongodb.net/?retryWrites=true&w=majority";
+
+
+const connection = mongoose.connect(connectionString).then(()=>{
+    console.log("Conncetion successful")
+}).catch((e)=>{
+    console.log(e);
+})
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+const connection = require("./configs/mongoDBconfigs");
+
+const PORT = 3000;
+
+
+app.listen(PORT,'0.0.0.0',()=>{
+    console.log("app listening on port ${PORT}")
+})
