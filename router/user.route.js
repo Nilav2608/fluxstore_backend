5ff69e3800cd1babc48ea190a9c117571de7d424
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-25 14:53:37 +0530

    login route

--- router/user.route.js
+++ router/user.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/registerUser',userController.register);
+router.post('/login',userController.login);
 
 
 
