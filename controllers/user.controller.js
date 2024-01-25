562feafd5e083d19481cb46d500ccc3493ef033b
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-25 14:53:25 +0530

    Login user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const UserServices = require("../services/user.services");
 const {request,response} = require('../app');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.register = async(request,response,next)=>{
 
@@ -34,4 +35,51 @@ exports.register = async(request,response,next)=>{
             }
         )
     }
+}
+
+exports.login = async (request,response,next)=>{
+     const {email,password} = request.body;
+
+     try {
+        if(!(email && password)){
+            response.status(400).json(
+                {
+                    status:false,
+                    message: "Email or password is empty!"
+                }
+            )
+        }
+        const user = await UserServices.checkUser(email);
+        if(!user){
+            response.status(401).json(
+                {
+                    status:false,
+                    message: "User does not exists"
+                }
+            )
+        }
+        const loginSuccessState = await UserServices.loginUser(email,password);
+
+        if (loginSuccessState) {
+            const tokenData = {
+                _id: user._id,
+                email: user.email
+            }
+            const secretKey = process.env.SECRET_KEY;
+            const expiry = process.env.EXPIRY;
+            const generatedToken = await UserServices.generateToken(tokenData,secretKey,expiry);
+            response.status(201).json({
+                status:true,
+                token: generatedToken,
+                message: "Logged in successfully"
+            })
+        }else if(loginSuccessState === false){
+            response.status(401).json({status:false,message:"Invalid email or password"})
+            console.log("Invalid email or password");
+        }else{
+            return response.status(500).json({status:false,message:"An error occurrd during Login"})
+        }
+     } catch (error) {
+        console.log(error);
+     }
 }
