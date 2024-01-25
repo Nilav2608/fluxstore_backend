ca37363fdcef7fa280b840bca3b920b66dccdd22
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-25 13:49:11 +0530

    Refactor routes in index.js

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT,'0.0.0.0',()=>{
     console.log("app listening on port ${PORT}")
 })
-
-app.get("/",(req,res)=>{
-    res.send("App is running successful");
-})
