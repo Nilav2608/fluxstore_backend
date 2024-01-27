891da7847059bda0a4ee2d588a61e7eba35743fe
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-27 23:21:26 +0530

    products route

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRouter = require("./router/user.route");
+const productsRouter = require('./router/products.route');
 const body_Parser = require('body-parser');
 const app = express();
 
 app.use(body_Parser.json())
 app.use('/',userRouter)
+app.use('/',productsRouter)
 
 module.exports = app;
