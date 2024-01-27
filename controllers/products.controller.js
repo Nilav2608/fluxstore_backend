0916b1a2499e499d99ee864cf13603c044dd8de6
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-28 01:23:24 +0530

    products controller addproducts

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -28,3 +28,51 @@ exports.getProducts = async(request,response,next)=>{
         console.log(error);
     }
 }
+
+//productName,imageUrl,price,descriptionn,ratings,sizes,colors,favorite
+
+exports.addProducts = async(request,response,next)=>{
+     const{productName,description,price,imageUrl,sizes,colors,favorite,ratings} = request.body;
+
+     try {
+        if (!(productName && imageUrl && price && description && ratings && sizes && colors && favorite)) {
+            response.status(400).json(
+                {
+                    status : false,
+                    message : "Invaid inputs, all the parameters are required"
+                }
+            )  
+         }
+         const newProduct = {
+            productName : productName,
+            imageUrl:imageUrl,
+            price:price,
+            description:description,
+            ratings:ratings,
+            sizes:sizes,
+            colors:colors,
+            favorite:favorite
+         }
+         console.log("---------------------------------------------");
+         console.log("From front end");
+         console.log(newProduct);
+    
+         const successState = await ProductCatalogServices.addProduct(newProduct);
+         console.log(successState);
+         if (successState) {
+            response.status(201).json(
+                {
+                    status : true,
+                    message:"New product added successfully"
+                }
+            )
+         }
+     } catch (error) {
+        response.status(400).json(
+            {
+                status : false,
+                message: error
+            }
+        )
+     }
+}
