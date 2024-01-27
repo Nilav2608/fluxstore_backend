475ddf8ecd80abd0bae5d06573759c0111de3181
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-27 23:20:15 +0530

    Add- mongoose Product schema

--- models/product.model.js
+++ models/product.model.js
@@ -2,3 +2,37 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 
+
+const productSchema = new Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  descriptionn: {
+    type: String,
+  },
+  ratings: {
+    type: Number,
+  },
+  sizes: {
+    type: [String],
+  },
+  colors: {
+    type: [String],
+  },
+  favorite: {
+    type: Boolean,
+  },
+});
+
+const Products = mongoose.model('Products', productSchema);
+
+module.exports = Products;
