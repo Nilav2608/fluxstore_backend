ee6776d8a0cb5a33b33072832a95021df817909e
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-29 00:43:22 +0530

    Cart property is sacked in the user model and new address property wille be implemented

--- models/user.models.js
+++ models/user.models.js
@@ -12,38 +12,10 @@ const userSchema = new Schema({
     },
     email: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
-    address:{
-      street: {
-        type: String,
-      },
-      city: {
-        type: String,
-      },
-      state: {
-        type: String,
-      },
-      postalCode: {
-        type: String,
-      },
-      country: {
-        type: String,
-      },
-      phoneNumber: {
-        type: String,
-      },
-    },
-    cartItems: [
-       {
-         productId: { type: Number, required: true },
-         productName: { type: String, required: true },
-         price:{type: Number,required : true},
-         size: { type: String,required : true },
-         color: { type: String,required : true },
-         imageURL: { type: String,required : true },
-         quantity: { type: Number, required: true },
-         selected:{type:Boolean,required : true}
-      },
-  ],
+    // address:{
+    //   type: addressSchema,
+    //   required: false},
+
 })
 
 userSchema.pre("save", async function () {
@@ -71,3 +43,32 @@ function next(data) {
 const UserModel = mongoose.model('users',userSchema,);
 
 module.exports = UserModel;
+
+
+
+ //   type: {
+    //     street: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     city: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     state: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     postalCode: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     country: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     phoneNumber: {
+    //         type: String,
+    //         required: true
+    //     },
+    // },
