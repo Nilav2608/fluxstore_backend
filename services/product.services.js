7549866d15fabace47a08043312e77ba48295b27
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-28 14:37:12 +0530

    add product mongoDB service

--- services/product.services.js
+++ services/product.services.js
@@ -13,6 +13,35 @@ class ProductCatalogServices{
             console.log(error);
         }
     }
+
+    static async addProduct(newData){
+        try {
+                const createProduct = await Products({
+                    productName : newData.productName,
+                    imageUrl:newData.imageUrl,
+                    price:newData.price,
+                    description:newData.description,
+                    ratings:newData.ratings,
+                    sizes:newData.sizes,
+                    colors:newData.colors,
+                    favorite:newData.favorite
+                })
+                await createProduct.save().then((result) => {
+                    console.log('Document inserted successfully:', result);
+                  })
+                  .catch((error) => {
+                    console.error('Error inserting document:', error);
+                  });
+                if (createProduct) {
+                    return true
+                }else{
+                    throw "Faild to insert data"
+                }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 
