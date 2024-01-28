99e10db077336d9db58fb6fc38a66913569c097a
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-28 14:39:19 +0530

    new add product route

--- router/products.route.js
+++ router/products.route.js
@@ -3,5 +3,6 @@ const productsController = require('../controllers/products.controller')
 
 
 router.get('/products',productsController.getProducts);
+router.post('/addProducts',productsController.addProducts);
 
 module.exports = router;
