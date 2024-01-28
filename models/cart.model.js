f1386f45ec8dc89f73b7608e01a0bc085609bfb7
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-29 00:39:56 +0530

    Add Cart model

---
+++ models/cart.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const cartItemsSchema = new Schema({
+    userId: { type: String, required: true },
+    productId: { type: String, required: true },
+    productName: { type: String, required: true },
+    price: { type: Number, required: true },
+    size: { type: String, required: true },
+    color: { type: String, required: true },
+    imageURL: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    selected: { type: Boolean, required: true }
+});
+
+
+const Cart = mongoose.model("Cart",cartItemsSchema);
+
+module.exports = Cart;
