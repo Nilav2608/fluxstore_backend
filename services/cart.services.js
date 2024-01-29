2a65c319633bc123b26a092bc5f16a0141839651
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-29 17:47:24 +0530

    delete cart from mongoDb

--- services/cart.services.js
+++ services/cart.services.js
@@ -37,6 +37,33 @@ class CartServices{
             
         }
     }
+
+    static async deleteFromCart(data) {
+  try {
+    const itemExists = await Cart.find({
+        _id: data.id
+    //   email: data.userId
+    });
+    if (itemExists) {
+      const deleteCartItem = await Cart.deleteOne({ _id: data.id });
+
+      if (deleteCartItem.deletedCount > 0) {
+        console.log("Item removed from cart successfully");
+        return true;
+      } else {
+        console.log("Failed to remove item from cart");
+        return false;
+      }
+    } else {
+      console.log("Item not found in the cart");
+      return false;
+    }
+  } catch (error) {
+    console.error("Error deleting item from cart:", error.message);
+    return false;
+  }
+}
+
 }
 
 module.exports = CartServices;
