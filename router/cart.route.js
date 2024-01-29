d14bdf61cbfc188afcca8d9b835ff8889a451d2e
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-29 17:47:51 +0530

    Delete route

--- router/cart.route.js
+++ router/cart.route.js
@@ -3,5 +3,6 @@ const cartController = require('../controllers/cart.controller');
 
 
 router.post('/AddToCart',cartController.addToCart)
+router.delete('/deleteCartItem',cartController.deleteFromCart)
 
 module.exports = router;
