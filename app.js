4bfb04b41c3c046d95b70b5a53c262d6d21310ea
Nilavarasu2608 <knlsu444@gmail.com>
2024-01-29 14:20:12 +0530

    New cart route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRouter = require("./router/user.route");
 const productsRouter = require('./router/products.route');
+const cartRouter = require('./router/cart.route');
 const body_Parser = require('body-parser');
 const app = express();
 
 app.use(body_Parser.json())
 app.use('/',userRouter)
 app.use('/',productsRouter)
+app.use('/',cartRouter)
 
 module.exports = app;
