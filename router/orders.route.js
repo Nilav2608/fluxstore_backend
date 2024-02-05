4d8d8239183bb41fe601022e4d59250753e1044b
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-05 14:24:06 +0530

    Spelling - refactor

--- router/orders.route.js
+++ router/orders.route.js
@@ -5,7 +5,7 @@ const ordersController = require('../controllers/orders.controller');
 router.get('/getMyOrders',ordersController.getMyOrders);
 router.post('/newOrder',ordersController.newOrderRequest);
 router.post('/cancelOrder',ordersController.cancelOrder);
-router.post('/admin/updateDelivaryStatus',ordersController.updateDelivaryStatus);
+router.post('/admin/updateDeliveryStatus',ordersController.updateDeliveryStatus);
 
 
 module.exports = router;
