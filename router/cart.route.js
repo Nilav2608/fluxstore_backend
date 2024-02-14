16272c1a798f4e24efb85413d904df55d60d6f77
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-15 01:10:51 +0530

    Add- New route

--- router/cart.route.js
+++ router/cart.route.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const cartController = require('../controllers/cart.controller');
 
 
-router.post('/AddToCart',cartController.addToCart)
-router.delete('/deleteCartItem',cartController.deleteFromCart)
+router.post('/api/v1/users/AddToCart',cartController.addToCart)
+router.post('/api/v1/users/getuserCart',cartController.getUserCartItems)
+router.delete('/api/v1/users/deleteCartItem',cartController.deleteFromCart)
 
 module.exports = router;
