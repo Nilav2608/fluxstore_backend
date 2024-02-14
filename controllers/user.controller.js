f913b91664330d5a6419cd285ebe03e3d45dc282
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-15 01:10:36 +0530

    Fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const UserServices = require("../services/user.services");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -9,6 +10,7 @@ exports.register = async(request,response,next)=>{
 
     if (!(userName && email && password)) {
         response.status(400).json({
+            status : true,
             success:"email or password is empty"
         })
     }
@@ -17,7 +19,7 @@ exports.register = async(request,response,next)=>{
         response.status(201).json(
             {
                 status : true,
-                message:"User registered successfully"
+                message:"Registeration Successful!"
             }
         )
     }else{
@@ -36,11 +38,67 @@ exports.register = async(request,response,next)=>{
     }
 }
 
-exports.login = async (request,response,next)=>{
-     const {email,password} = request.body;
+exports.login = async (request,response)=>{
+    const {email,password} = request.body;
 
-     try {
+    try {
         if(!(email && password)){
+            return response.status(400).json({
+                status:false,
+                message: "Email or password is empty!"
+            });
+        }
+
+        const user = await UserServices.checkUser(email);
+        if(!user){
+            return response.status(401).json({
+                status:false,
+                message: "User does not exist"
+            });
+        }
+
+        const loginSuccessState = await UserServices.loginUser(email,password);
+
+        if (loginSuccessState) {
+            const tokenData = {
+                _id: user._id,
+                email: user.email
+            };
+            const secretKey = process.env.SECRET_KEY;
+            const expiry = process.env.EXPIRY;
+            const generatedToken = await UserServices.generateToken(tokenData,secretKey,expiry);
+            return response.status(201).json({
+                status:true,
+                token: generatedToken,
+                message: "Logged in successfully"
+            });
+        } else {
+            return response.status(401).json({
+                status:false,
+                message: "Invalid email or password"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+            status:false,
+            message:"An error occurred during Login"
+        });
+    }
+}
+
+
+exports.getUser =  async (request,response) =>{
+    const {userId} = request.body;
+
+    try {
+        if(userId){
+            const results = await UserServices.getUserDetails(userId);
+            response.status(201).json({
+                status:true,
+                data: results
+            })
+        }else{
             response.status(400).json(
                 {
                     status:false,
@@ -48,37 +106,13 @@ exports.login = async (request,response,next)=>{
                 }
             )
         }
-        const user = await UserServices.checkUser(email);
-        if(!user){
-            response.status(401).json(
-                {
-                    status:false,
-                    message: "User does not exists"
-                }
-            )
-        }
-        const loginSuccessState = await UserServices.loginUser(email,password);
 
-        if (loginSuccessState) {
-            const tokenData = {
-                _id: user._id,
-                email: user.email
+    } catch (error) {
+        response.status(400).json(
+            {
+                status:false,
+                message: error.message
             }
-            const secretKey = process.env.SECRET_KEY;
-            const expiry = process.env.EXPIRY;
-            const generatedToken = await UserServices.generateToken(tokenData,secretKey,expiry);
-            response.status(201).json({
-                status:true,
-                token: generatedToken,
-                message: "Logged in successfully"
-            })
-        }else if(loginSuccessState === false){
-            response.status(401).json({status:false,message:"Invalid email or password"})
-            console.log("Invalid email or password");
-        }else{
-            return response.status(500).json({status:false,message:"An error occurrd during Login"})
-        }
-     } catch (error) {
-        console.log(error);
-     }
+        )
+    }
 }
