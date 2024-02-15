6854ba790c83fbf7b41c661e42c773262c6c5b83
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-16 00:57:59 +0530

    New  - orders route

--- router/orders.route.js
+++ router/orders.route.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const ordersController = require('../controllers/orders.controller');
 
 
-router.get('/getMyOrders',ordersController.getMyOrders);
-router.post('/newOrder',ordersController.newOrderRequest);
-router.post('/cancelOrder',ordersController.cancelOrder);
-router.post('/admin/updateDeliveryStatus',ordersController.updateDeliveryStatus);
+router.get('/api/v1/users/getUserOrders',ordersController.getMyOrders);
+router.post('/api/v1/users/newOrder',ordersController.newOrderRequest);
+router.post('/api/v1/users/cancelOrder',ordersController.cancelOrder);
+router.post('/api/v1/admin/updateDeliveryStatus',ordersController.updateDeliveryStatus);
 
 
 module.exports = router;
