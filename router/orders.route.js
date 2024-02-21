5fa03b71d0134eabdb889fd091a5c847f5034113
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-22 00:02:17 +0530

    New request method changed

--- router/orders.route.js
+++ router/orders.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const ordersController = require('../controllers/orders.controller');
 
 
-router.get('/api/v1/users/getUserOrders',ordersController.getMyOrders);
+router.post('/api/v1/users/getUserOrders',ordersController.getMyOrders);
 router.post('/api/v1/users/newOrder',ordersController.newOrderRequest);
-router.post('/api/v1/users/cancelOrder',ordersController.cancelOrder);
+router.put('/api/v1/users/cancelOrder',ordersController.cancelOrder);
 router.post('/api/v1/admin/updateDeliveryStatus',ordersController.updateDeliveryStatus);
 
 
