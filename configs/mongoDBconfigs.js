c878c4aed726febe0b8ac991f0b01768d846705c
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-11 12:08:47 +0530

    Refactored mongo connection string

--- configs/mongoDBconfigs.js
+++ configs/mongoDBconfigs.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectionString = process.env.CONNECTION_STRING;
 
 
-const connection = mongoose.connect(connectionString).then(()=>{
+const connection = mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dxzadmr.mongodb.net/Fluxstore?retryWrites=true&w=majority`).then(()=>{
     console.log("Database Connection successful")
 }).catch((e)=>{
     console.log(e);
